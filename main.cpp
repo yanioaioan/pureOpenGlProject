063630113bb4f03b7a65aea91ab21d2f290fc293
Yannis Ioannidis <yanioaioan@hotmail.com>
2016-10-31 16:24:29 +0000

    updated to initialize Glew after creating a valid openGL rendering context

--- main.cpp
+++ main.cpp
@@ -15,12 +15,16 @@
 
 #include <iostream>
 
+// An array of 3 vectors which represents 3 vertices
+static const GLfloat g_vertex_buffer_data[] = {
+   -0.3f, -0.3f, 0.0f,
+   0.3f, -0.3f, 0.0f,
+   0.0f,  0.3f, 0.0f,
+};
+
 int main(int argc, char *argv[])
 {
-    if(glewInit())
-    {
-        std::cout<<"glew init"<<std::endl;
-    }
+
 
     //creation of the rendering window
     SDL_Window* window = NULL;
@@ -29,13 +33,27 @@ int main(int argc, char *argv[])
     //SDL init
     SDL_Init( SDL_INIT_VIDEO );
     //Use OpenGL 3.3
-    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 3 );
+    SDL_GL_SetAttribute( SDL_GL_CONTEXT_MAJOR_VERSION, 4 );
     SDL_GL_SetAttribute( SDL_GL_CONTEXT_MINOR_VERSION, 3 );
 
     //create opengl window
     window = SDL_CreateWindow( "test opengl", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 400, 400, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN );
+
+
     //create a valid opengl context
     openglContext = SDL_GL_CreateContext( window );
+
+    //First you need to create a valid OpenGL rendering context and call glewInit() to initialize the extension entry points.
+    //initialize glew after SDL_CreateWindow & SDL_GL_CreateContext otherwise it crashes
+    GLenum err = glewInit();
+    if (GLEW_OK != err)
+    {
+      /* Problem: glewInit failed, something is seriously wrong. */
+      fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+
+    }
+    fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+
 
     SDL_Event e;
     bool exit=false;
@@ -48,10 +66,13 @@ int main(int argc, char *argv[])
                exit = true;
            }
 
+
+
             //Clear color buffer
             glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
             glClearColor( .4f, .4f, .1f, 1 );
+
+
 
             //swap the the sdl window update our screen
             SDL_GL_SwapWindow( window );
@@ -59,9 +80,6 @@ int main(int argc, char *argv[])
     }
 
 
-    GLuint *unitilializedBuffer;//gl integer
-    glGenBuffers(1, unitilializedBuffer);
-    glDeleteBuffers(1,unitilializedBuffer);
 
     return 0;
 }
